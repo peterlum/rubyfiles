2f4e09ac667308c51b56622e786757303cfe49f8
PeterLum <hello@peterlum.co>
2014-10-06 13:58:52 -0400

    second commit

---
+++ test.rb
@@ -0,0 +1,216 @@
+puts "Hello World"
+
+def onlyAmerica(thing)
+	put thing + " Only in America!"
+end
+
+numberArray=[0,500,4,1000]
+   numberArray.each do |number|
+   	if number > newNumber
+   		newNumber=number
+   	end
+   end
+
+   return newNumber
+
+   array1=[:toyota, :tesla]
+   array2=["Prius","Model S"]
+
+   #def hashCars
+   #array1.each do |carmodels|
+   	#carmodels=hash{array1.each do + array2.each do}
+
+def hashCars(CompanyCars)
+	array1.each do |Companies|
+		array2.each do|Cars|
+			hashPairs={"Companies":"Cars"}
+			puts hashPairs
+		end
+	end
+end
+
+   		
+
+=begin
+
+***solution to number 2
+def max_loop(array)
+	max_so_far=nil
+
+	array.each do|item|
+		if !max_so_far || (max_so_far<item)
+			max_so_far=item
+			end
+			end
+			max_so_far
+			end
+
+***end of solution of number 2
+
+combine("cats","Only in America!")
+end
+
+def onetoonehundred(ahundred)
+	some_number=0
+	while some_number<101
+			puts some_number
+			if some_number/3=Fixnum
+				puts "Fizz"
+			elsif some_number/5=Fixnum
+			elsif some_number/15=Fixnum
+			else puts some_number
+				
+			end
+		end
+	end
+end
+
+
+
+def numbers(hundred)
+	for i (1..100)
+	puts i
+	if i/3=Fixnum
+		puts"Fizz"
+		elsif condition
+		 	 i/5=Fixnum
+			puts"Buzz"
+			elsif i/15=Fixnum
+				puts"Fizzbuzz"
+			else puts i
+			end
+		end
+	end
+end
+
+
+lo=1
+hi=5
+for i in (lo..hi)
+puts i
+end
+
+("a".."z") .each do |letter|
+puts letter
+
+
+
+for i (1..100)
+	puts i
+	if i/3 == Fixnum
+		puts "Fizz"
+	elsif false
+		puts i
+	elsif i/5 == Fixnum
+		puts "Buzz"
+	elsif false
+		puts i
+	elsif i/15 == Fixnum
+		puts "Fizzbuzz"
+	elsif false
+		puts i
+	end
+end
+
+*returns end a function or method there, and wanything afterward do not compile
+
+* one solution to number 3
+
+arrayCompanies=[:Toyota, :Tesla]
+arrayCars=["Prius", "Model S"]
+
+hash={}
+
+for i in (0..arrayCompanies.length)
+	puts arrayCompanies[i]
+	puts models[i]
+	end
+
+	for i in [0..arrayCompanies.length]
+		puts arrayCompanies[i]
+		puts arrayCars[i]
+		end
+
+array=[]
+
+array=[1,2,3]
+array[0]="fred"
+
+array=["fred",2,3]
+
+my_hash=[]
+my hash[hi]="bye"
+
+***solutions to 3****
+
+def makeHash(keys,values)
+	hash={}
+	for i in (0...keys.length)
+		key=keys[i]
+		value=values[i]
+		hash[key]=value
+	end
+	hash
+	end
+
+
+... ellipsis includes everything within a range except the last member of an array, string, or variable
+
+***solution to the last one****
+def FizzBuzz
+	for i in (1..100)
+		if (i%3 == 0 ) && ( i%5 == 0)
+			puts "FizzBuzz"
+			elsif (i%3 == 0 )
+			puts "Fizz"
+			elsif ( i%5 == 0)
+			puts "Buzz"
+			else
+				puts i
+				end
+				end
+
+=end
+
+
+
+
+#has to check the last condition first and must match several conditions since multiples of 3 and 5 will override multiples of 15
+
+arrayNumbers=(0..100)
+
+def fizzingAndbuzzing(numz)
+	arrayNumbers.each do |phrase|
+	phrase = array[0]
+	puts phrase
+	if phrase%15 == Fixnum
+		puts "FizzBuzz"
+	elsif phrase%3 == Fixnum
+		puts "Fizz"
+	elsif phrase&5 == Fixnum
+		puts "Buzz"
+	else puts phrase
+	end
+end
+
+
+string1="string one"
+string2="string two"
+
+def makeHash=(key,value)
+	hash{}
+	key=keys
+	value=values
+	hash={keys=>values}
+end
+			
+		
+		
+		
+			
+
+
+
+
+
+
