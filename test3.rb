5d4680243660c5f0e29282c4335f1da62fd8586b
PeterLum <hello@peterlum.co>
2014-10-07 23:17:33 -0400

    New Commit

---
+++ test3.rb
@@ -0,0 +1,82 @@
+stringText="it is what it is and that is all it is"
+
+words=stringText.split
+
+puts words
+
+word_counts={}
+
+words.each do|word|
+	if word_counts.has_key?(word)
+	word_counts[word] = word_counts[word] + 1
+	else
+	word_counts[word]=1
+end
+puts word_counts
+end
+
+
+def words.each do|word|
+	if word_counts.has_key?(word)
+	word_counts[word] = word_counts[word] + 1
+	else
+	word_counts[word]=1
+end
+
+puts word_counts
+end
+
+def findMaxwordcount(hash)
+	hash.max_by{|k,v| v}
+#uses a method that looks through the established keys, K, and values, V, and returns the key with its value in enum form
+end
+
+
+
+=begin def wordCount=(array)
+	count=0
+	array.each do |words|
+		for words.each 
+
+word_counts=0
+	while word_counts=<word.length
+
+
+def wordCounter(array[""])
+	count=0
+	while count=< array.length
+		  if array[""]=array[""+1]
+			count= count+1
+			else puts count
+			end
+		end
+	end
+end
+
+
+
+wordArray.each do |words|
+	words=keys
+	words.=values
+
+
+
+def wordCounting(array)
+	array.each do |words|
+		puts words.count
+	end
+end
+
+
+
+
+
+
+def makeTexthash(key,value)
+	hash{}
+	key=keys
+	value=values
+	hash={keys=>values}
+end
+
+=end
